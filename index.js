7fe76b6f206b3e608bccbba35f5dc31cd9253f62
Oscar <oscar_ochoac@yahoo.com>
2019-04-08 23:33:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays(){
 
 function happyHolidaysTo(name){
 
-  return '\'Happy holidays, \'${name}\'!\''
+  return happyHolidays().${name}
 }
 /*
 function happyHolidayto(holiday, name){
